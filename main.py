4c41f4309700bd6ea2bf93184be404328162b35d
Vsevolod <skimin1992@hotmail.com>
2025-04-01 22:04:36 +0400

    Programmed Player Rotation

--- main.py
+++ main.py
@@ -3,22 +3,28 @@
 # throughout this file
 import pygame
 from constants import *
-
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock_object = pygame.time.Clock()
+    player_object = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
-            
-
-            
+              
         screen.fill("black", rect=None, special_flags=0) 
+        player_object.update(dt)
+        player_object.draw(screen)
         pygame.display.flip()
+        dt = clock_object.tick(60) / 1000
+
     
     
     print("Starting Asteroids!")
